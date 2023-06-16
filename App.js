c59d1cc51b0fb23b8ba5f3efc5a57fbe6b9cd79f
vikas <vikassoni247@gmail.com>
2023-06-16 15:42:32 +0530

    This is my test

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Dropdown from './src/component/Dropdown/Dropdown';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Dropdown />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/component/Dropdown/Dropdown.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Dropdown = ({data = [], label}) => {
+  return (
+    <View>
+      <Text>Dropdown</Text>
+    </View>
+  );
+};
+
+export default Dropdown;
